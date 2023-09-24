10328a6c322d80eac569a6cc5f6e3995988e8fbb
avpogos2 <storyofspolding@gmail.com>
2023-09-24 17:35:09 +0300

    Added Timber logger library.

--- app/src/main/java/com/example/breakingworldnewsapp/presentation/app/App.kt
+++ app/src/main/java/com/example/breakingworldnewsapp/presentation/app/App.kt
@@ -2,6 +2,14 @@ package com.example.breakingworldnewsapp.presentation.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class App: Application()
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
